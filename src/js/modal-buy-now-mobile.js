14d1df7c8630de3520ab8067414098c88f3fce9d
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-08-26 02:00:41 +0300

    buy now modal completed

---
+++ src/js/modal-buy-now-mobile.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-mobile-buy]'),
+    closeModalBtn: document.querySelector('[data-modal-close-mobile-buy]'),
+    modal: document.querySelector('[data-modal-mobile-buy]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-buy');
+  }
+
+  
+})();
