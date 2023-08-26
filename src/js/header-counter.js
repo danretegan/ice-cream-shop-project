cf2a4024062af118e61c645be3aa1e395139ac07
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-08-26 12:35:34 +0300

    Added counter animation to header

---
+++ src/js/header-counter.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count1", 0, 16, 1500);
+  counter("count2", 0, 23, 1950);
+ });
+ 
