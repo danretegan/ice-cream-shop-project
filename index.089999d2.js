c477dedb23bedcef51f1fe9d017ea20d93344f13
danretegan <96877914+danretegan@users.noreply.github.com>
2023-12-22 10:25:16 +0000

    Deploying to gh-pages from @ danretegan/ice-cream-shop-project@555709b21dc831daac05d9da396074dac66967c1 🚀

--- index.089999d2.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-modal-open-location]"),closeModalBtn:document.querySelector("[data-modal-close-location]"),modal:document.querySelector("[data-modal-location]")};function e(){o.modal.classList.toggle("is-hidden-location")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.089999d2.js.map
