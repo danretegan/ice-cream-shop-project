555709b21dc831daac05d9da396074dac66967c1
Dan Retegan <danretegan@yahoo.com>
2023-12-22 12:24:43 +0200

    disable bodyScrollLock

--- src/js/scroll-button.js
+++ src/js/scroll-button.js
@@ -1,5 +1,5 @@
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let mybutton = document.getElementById('btn-back-to-top');
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -7,17 +7,14 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.display = 'none';
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+mybutton.addEventListener('click', backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
