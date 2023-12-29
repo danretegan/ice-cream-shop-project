5eafbbecaf60e71d0f846ffc8b13ccd171f26d02
Dan Retegan <danretegan@yahoo.com>
2023-12-29 13:02:31 +0200

    fixedbutton & link how its made button unification

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -25,6 +25,6 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
